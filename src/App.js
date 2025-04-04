0dd634a0c6a47d749f1ea39298319a4deb0c8ec7
Vishesh009 <Visheshbajpai9@gmail.com>
2025-04-05 04:23:21 +0530

    Bottom Floating Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import Header from "../components/Header";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6 __variable_d65c78">
+      <h1 className="flex flex-col min-h-[100dvh] space-y-10">
+        Hi, I'm Shivam 👋 Senior Software Engineer with a passion for building
+        innovative solutions. Active on social media, sharing insights and
+        projects.
+      </h1>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
