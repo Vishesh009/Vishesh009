3032bcd3165ce4aa89d281f3b0d22cc999c6004f
Vishesh009 <Visheshbajpai9@gmail.com>
2025-04-07 03:18:45 +0530

    Will be able to deploy it

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ import Contact from "../components/Contact";
 const App = () => {
   return (
     <div className="min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6 __variable_d65c78">
+      {/* <h1 className="flex flex-col min-h-[100dvh] space-y-10">
+        Hi, I'm Vishesh 👋 Software Engineer with a passion for building
+        innovative solutions. Active on social media, sharing insights and
+        projects.
+      </h1> */}
       <Hero />
       <About />
       <WorkExperience />
